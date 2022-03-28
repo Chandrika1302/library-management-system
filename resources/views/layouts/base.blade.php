9e9f31a0e530423c9ad4e428b04d1e0c023c0db7
chandrika <gehini1302@gmail.com>
2022-03-28 12:58:23 +0530

    add tailwind css

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -7,25 +7,51 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <title>Library Store</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+
+
 </head>
 
-<body>
-    <header>
-        <h1> Welcome {{session('user')}}!</h1>
-        <nav>
-            <a href="/">Home</a><a href="/logout">Logout</a>
+<body class="bg-gradient-to-l from-cyan-500 to-blue-500 font-sans">
+    <header
+        class="flex justify-between bg-opacity-50 bg-white backdrop-blur-md shadow-xl  text-center text-xl font-medium py-4 px-10">
+        <h1 class="text-5xl p-4">
+            Welcome {{session('user')}}!
+        </h1>
+        <nav class="mt-6">
+            <a href="/"
+                class="shadow-xl rounded-md bg-opacity-50 bg-white backdrop-blur-md cursor-pointer transform hover:scale-110 active:scale-90 duration-100 p-4">
+                Home</a>
+            <a href="/logout"
+                class="shadow-xl rounded-md bg-white backdrop-blur-md rounded-md bg-opacity-50 cursor-pointer transform hover:scale-110 active:scale-90 duration-100 p-4">Logout</a>
         </nav>
     </header>
     <main>
-        <ul class="side-bar">
-                  <li> <a href="/authors">All Authors</a> </li>
-                  <li> <a href="/books">All Books</a> </li>
-                  <li> <a href="/about">About</a> </li>
-                  <li> <a href="/author/create">Add new Author</a> </li>
-                  <li> <a href="/book/create">Add new Book</a> </li>
+        <ul
+            class="side-bar shadow-xl bg-opacity-50 bg-white backdrop-blur-md p-4 cursor-pointer w-64 text-xl font-medium">
+            <li
+                class="shadow-xl rounded-md bg-opacity-50  backdrop-blur-md p-4 cursor-pointer transform hover:scale-110 active:scale-90 duration-100">
+                <a href="/authors">All Authors</a>
+            </li>
+            <li
+                class="shadow-xl rounded-md bg-opacity-50 backdrop-blur-md p-4 cursor-pointer transform hover:scale-110 active:scale-90 duration-100">
+                <a href="/books">All Books</a>
+            </li>
+            <li
+                class="shadow-xl rounded-md bg-opacity-50 backdrop-blur-md p-4 cursor-pointer transform hover:scale-110 active:scale-90 duration-100">
+                <a href="/about">About</a>
+            </li>
+            <li
+                class="shadow-xl rounded-md bg-opacity-50 backdrop-blur-md p-4 cursor-pointer transform hover:scale-110 active:scale-90 duration-100">
+                <a href="/author/create">Add new Author</a>
+            </li>
+            <li
+                class="shadow-xl rounded-md bg-opacity-50 backdrop-blur-md p-4 cursor-pointer transform hover:scale-110 active:scale-90 duration-100 ">
+                <a href="/book/create">Add new Book</a>
+            </li>
         </ul>
         <div class="app">
-        @yield('content')
+            @yield('content')
         </div>
     </main>
 </body>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,8 +2,10 @@
 
 @section('content')
 
-<h2> Welcome to Online Library </h2>
-<ul>
+<h2
+    class="text-5xl text-center py-24 font-extrabold text-transparent bg-clip-text bg-gradient-to-b via-slate-700 to-slate-800 w-full">
+    Welcome to Online Library </h2>
+<ul class="text-xl">
     <li>The Library Has {{$authorsCount}} Authors</li>
     <li>The Library contains {{$booksCount}} Books</li>
 </ul>
