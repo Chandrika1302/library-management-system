09b328d589546fd134ad9054921f6a00966e4df2
chandrika <gehini1302@gmail.com>
2022-03-28 09:53:27 +0530

    add tailwind css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome</title>
     <link rel="stylesheet" href="css/style.css">
-
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-    <h1 class="welcome">Welcome to online Library, please login or register</h1>
-    <div class="login-links">
-       <a href="/register">Register</a>
-       <a href="/login">Login</a> 
+
+<body class="bg-gradient-to-l from-cyan-500 to-blue-500">
+    <div
+        class="m-10 rounded-md bg-opacity-50 bg-white backdrop-blur-md shadow-xl grid grid-cols-2 justify-items-center p-4 font-sans font-medium">
+        <div class="shadow-xl rounded-md bg-opacity-50 backdrop-blur-md text-4xl p-4 font-bold mr-80 rounded-md">Library
+        </div>
+        <div class="flex flex-end gap-10 text-3xl">
+            <a href="/register"
+                class="shadow-xl rounded-md bg-opacity-50 backdrop-blur-md p-4 cursor-pointer transform hover:scale-110 active:scale-90 duration-100">Register</a>
+            <a href="/login"
+                class="shadow-xl  rounded-md bg-opacity-50 backdrop-blur-md p-4 cursor-pointer transform hover:scale-110 active:scale-90 duration-100">Login</a>
+        </div>
     </div>
+    <h2
+        class='text-9xl text-center py-44 font-extrabold text-transparent bg-clip-text bg-gradient-to-b via-slate-700 to-slate-800'>
+        Welcome to Online Library Management System</h2>
+
 </body>
+
 </html>
